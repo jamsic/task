4e4945eb3b566791b49a4eb4a852cfc7e94b8016
jamsic <jamsic@users.noreply.github.com>
2017-08-17 23:33:03 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(name='module',
       version='0.1',
       description='module',
       url='http://github.com/jamsic/task',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      author='jamsic',
+      author_email='jamsic@yandex.ru',
       packages=find_packages(),
       package_data={'module': ['parser/keywords_parser/keywords_provider/data/techs_keywords.csv', 'crawler/data/data.db', 'urlprovider/data/urls.txt']},
       install_requires=[
-          'nltk', 'beautifulsoup4', 'requests', 'numpy'
+          'nltk', 'requests', 'numpy', 'bs4'
       ],
       zip_safe=False)
